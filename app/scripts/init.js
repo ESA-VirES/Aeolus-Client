80471b092295224a5da4bf06987c91b1556862be
santilland <daniel.santillan@eox.at>
2019-03-14 17:37:17 +0100

    Incremented cache bust

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -5,7 +5,7 @@
 
     root.require.config({
         // Update for each release
-        urlArgs: 'bust=v1.21',
+        urlArgs: 'bust=v1.22',
 
         waitSeconds: 120,
         /* starting point for application */
