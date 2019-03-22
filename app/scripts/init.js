dd5c40edefc46b4a6c4813b30e92dbdecdf1e459
santilland <daniel.santillan@eox.at>
2019-03-22 14:54:08 +0100

    Updated version to 1.3

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -5,7 +5,7 @@
 
     root.require.config({
         // Update for each release
-        urlArgs: 'bust=v1.28',
+        urlArgs: 'bust=v1.3',
 
         waitSeconds: 120,
         /* starting point for application */
