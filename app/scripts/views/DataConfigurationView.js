15fe02c00afbda78b5617a0f6f858325d8ef3c3f
santilland <daniel.santillan@eox.at>
2019-12-09 16:30:29 +0100

    Selection of product type implemented, extended parameter item to show extent, filterextent and colorscale; added filter functionality

--- app/scripts/views/DataConfigurationView.js
+++ app/scripts/views/DataConfigurationView.js
@@ -22,7 +22,9 @@
           type: 'handlebars',
           template: dataConfigurationTmpl
       },
-      initialize: function(options) {},
+      initialize: function(options) {
+        this.currentSelection = null;
+      },
       onShow: function(view){
 
         this.$('.close').on("click", _.bind(this.onClose, this));
@@ -32,37 +34,82 @@
           handle: '.panel-heading'
         });
 
+        var productId = null;
+        var that = this;
         // Load list of available product types
         globals.products.each(function(product){
-          this.$('#productbtnGroup').append(
-            '<button type="button" class="btn btn-default" value="'+
+
+          var button = $(
+            '<button type="button" class="btn btn-default productSelect" value="'+
             product.get('download').id+'"">'+
               product.get('name')+
             '</button>'
           );
+          this.$('#productbtnGroup').append(button);
+          if(product.get('visible')){
+            that.currentSelection = product.get('download').id;
+            button.addClass('active');
+          }
         }, this);
 
-        // Load parameters for selected product type
-        var selected = 'ALD_U_N_1B';
+        this.renderProductParameterList(false);
+
+        $('.productSelect').click(function(evt){
+          $('.productSelect').removeClass('active');
+          that.currentSelection = this.value;
+          that.renderProductParameterList(false);
+        });
+
+        $('#parameterSearchInput').off();
+        $('#parameterSearchInput').keyup(function(){
+          that.renderProductParameterList(this.value);
+        });
+      },
+
+      renderProductParameterList: function(filter){
         this.$('#parameterList').empty();
+        // Load parameters for selected product type
         var selectedProduct = null;
-        globals.products.forEach(function(p){
-          if(p.get('download').id === selected){
-            selectedProduct = p;
+        var currentId = this.currentSelection;
+        globals.products.each(function(product){
+          if(product.get('download').id === currentId){
+            selectedProduct = product;
           }
         });
-        if (selectedProduct !== null){
+
+        if(selectedProduct !== null){
           var parameterList = selectedProduct.get('download_parameters');
           for (var key in parameterList){
-            this.$('#parameterList').append(parameterItemTmpl({
-              key: key,
-              name: parameterList[key].name,
-              uom: parameterList[key].uom,
-            }))
+
+            // TODO: probably info should be kept somehow connected to product
+            // right now information for all parameters is global, need to 
+            // reconsider this approach
+            var parInfo = globals.dataSettings[key];
+            var show = true;
+            if(filter!==false && (
+                  key.toLowerCase().indexOf(filter.toLowerCase()) === -1 &&
+                  parameterList[key].name.toLowerCase().indexOf(filter.toLowerCase()) === -1 )){
+              show = false;
+            }
+            if(show){
+              if(parInfo !== undefined){
+                this.$('#parameterList').append(parameterItemTmpl({
+                  key: key,
+                  name: parameterList[key].name,
+                  uom: parameterList[key].uom,
+                  extent: defaultFor(parInfo.extent, null),
+                  filterExtent: defaultFor(parInfo.filterExtent, null),
+                  colorscale: defaultFor(parInfo.colorscale, 'viridis')
+                }))
+              } else {
+                console.log('Warning: parameter not defined: '+key);
+              }
+            }
           }
         }
-//"L1B_start_time_obs": {"uom": null, "name": "Start date and time of Observation (UTC)"},
+
       },
+
       onClose: function() {
         Communicator.mediator.trigger("ui:close", "download");
         this.close();
